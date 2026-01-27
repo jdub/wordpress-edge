a6661e5e22f726c7d397dc33375f7f19b6b48874
Jeff Waugh <jdub@bethesignal.org>
2026-01-28 00:56:00 +1100

    okay

--- .github/actions/php/opcache.sh
+++ .github/actions/php/opcache.sh
@@ -6,8 +6,8 @@ php composer.phar install --no-dev
 
 find /opt/zero/vendor /opt/zero/wordpress -name '*.php' -type f | xargs -n1 -P$(nproc) php \
   -d "memory_limit=-1" \
-  -d "opcache.enable_cli=1" \
+  -d "opcache.enable_cli=true" \
   -d "opcache.jit=function" \
   -d "opcache.file_cache=/opt/zero/opcache" \
-  -d "opcache.file_cache_only=1" \
+  -d "opcache.file_cache_only=true" \
   -r 'echo "."; opcache_compile_file($argv[1]);'
