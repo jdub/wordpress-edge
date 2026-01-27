d625276af812f100b61a1ce0e8a439b587c94b68
Jeff Waugh <jdub@bethesignal.org>
2026-01-28 00:01:10 +1100

    lknasdnasd

--- .github/actions/php/opcache.sh
+++ .github/actions/php/opcache.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-mkdir -p opcache
-ln -v -s /github/workspace /opt/zero
+ln -v -s . /opt/zero
+mkdir -p /opt/zero/opcache
+
+php composer.phar install --no-dev
+
 find /opt/zero/vendor /opt/zero/wordpress -name '*.php' -type f | xargs -n1 -P$(nproc) php \
   -d "memory_limit=-1" \
   -d "opcache.enable_cli=1" \
