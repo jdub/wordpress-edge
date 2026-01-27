ccbcd9686f43bd68d7705f9fdbde9d5e29f111e1
Jeff Waugh <jdub@bethesignal.org>
2026-01-27 21:06:08 +1100

    try an action

--- ci/opcache.sh
+++ ci/opcache.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-echo "Priming opcode cache"
-
-export LAMBDA_TASK_ROOT=$(pwd)
-export PHP_INI_SCAN_DIR=$LAMBDA_TASK_ROOT/lib:$LAMBDA_TASK_ROOT/lib/php-7.1.d
-
-mkdir -p $LAMBDA_TASK_ROOT/lib/cache
-
-find -name '*.php' | grep -v random_compat/random_ | xargs -n1 -P4 php \
-  -d memory_limit=-1 -d opcache.file_cache_only=0 \
+CACHE=zero/opcache
+mkdir -p ${CACHE}
+find vendor wordpress -name '*.php' -type f | xargs -n1 -P$(nproc) php -n \
+  -d "memory_limit=-1" \
+  -d "zend_extension=opcache" \
+  -d "opcache.enable_cli=1" \
+  -d "opcache.jit=function" \
+  -d "opcache.file_cache=$(pwd)/${CACHE}" \
+  -d "opcache.file_cache_only=1" \
   -r 'echo "."; opcache_compile_file($argv[1]);'
-
-CACHEDIR=$(echo $LAMBDA_TASK_ROOT/lib/cache/*)
-
-chmod -R go+rX $CACHEDIR
-mkdir -p $CACHEDIR/var/lib/task
-mv $CACHEDIR/codebuild/output/*/src/* $CACHEDIR/var/lib/task/
