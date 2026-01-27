3f65c57b4b2e06469c50a683a00e2c8f0fb234d0
Jeff Waugh <jdub@bethesignal.org>
2026-01-27 23:50:29 +1100

    blurp

---
+++ .github/actions/php/opcache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ln -s . /opt/zero
+mkdir -p /opt/zero/opcache
+find /opt/zero/vendor /opt/zero/wordpress -name '*.php' -type f | xargs -n1 -P$(nproc) php -n \
+  -d "memory_limit=-1" \
+  -d "zend_extension=opcache" \
+  -d "opcache.enable_cli=1" \
+  -d "opcache.jit=function" \
+  -d "opcache.file_cache=/opt/zero/opcache" \
+  -d "opcache.file_cache_only=1" \
+  -r 'echo "."; opcache_compile_file($argv[1]);'
