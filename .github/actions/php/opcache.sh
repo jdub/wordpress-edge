97cd35c3996a4dad374f72c0e1fb09789cde6782
Jeff Waugh <jdub@bethesignal.org>
2026-01-27 23:54:36 +1100

    asdasd

--- .github/actions/php/opcache.sh
+++ .github/actions/php/opcache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ln -s . /opt/zero
+ln -s /github/workspace /opt/zero
 mkdir -p /opt/zero/opcache
-find /opt/zero/vendor /opt/zero/wordpress -name '*.php' -type f | xargs -n1 -P$(nproc) php -n \
+find /opt/zero/vendor /opt/zero/wordpress -name '*.php' -type f | xargs -n1 -P$(nproc) php \
   -d "memory_limit=-1" \
   -d "zend_extension=opcache" \
   -d "opcache.enable_cli=1" \
