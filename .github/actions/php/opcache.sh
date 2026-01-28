6f45543d4d916adf091bd8120a0df15131ce42f0
Jeff Waugh <jdub@bethesignal.org>
2026-01-28 11:51:37 +1100

    oh come on

--- .github/actions/php/opcache.sh
+++ .github/actions/php/opcache.sh
@@ -3,9 +3,10 @@ set -euo pipefail
 
 php composer.phar install --no-dev
 
+mkdir -p /opt/zero
 mount -o bind $(pwd) /opt/zero
-mkdir -p /opt/zero/opcache
 
+mkdir -p /opt/zero/opcache
 find vendor wordpress -name '*.php' -type f | xargs -n1 -P$(nproc) php \
   -d "memory_limit=-1" \
   -d "opcache.enable_cli=true" \
