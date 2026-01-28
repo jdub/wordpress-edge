da9aece9a55a38d15e3d4effa91d9858be514985
Jeff Waugh <jdub@bethesignal.org>
2026-01-28 11:48:51 +1100

    bind theory

--- .github/actions/php/opcache.sh
+++ .github/actions/php/opcache.sh
@@ -3,14 +3,16 @@ set -euo pipefail
 
 php composer.phar install --no-dev
 
-mkdir -p opcache
+mount -o bind $(pwd) /opt/zero
+mkdir -p /opt/zero/opcache
+
 find vendor wordpress -name '*.php' -type f | xargs -n1 -P$(nproc) php \
   -d "memory_limit=-1" \
   -d "opcache.enable_cli=true" \
   -d "opcache.jit=function" \
-  -d "opcache.file_cache=$(pwd)/opcache" \
+  -d "opcache.file_cache=/opt/zero/opcache" \
   -d "opcache.file_cache_only=true" \
   -r 'echo "."; opcache_compile_file($argv[1]);'
 
 # fix opcache permissions
-chmod -R go+rX opcache
+chmod -R go+rX /opt/zero/opcache
